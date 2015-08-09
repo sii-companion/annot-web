7ee734ae630dc2140fd26b8ee30d3fcb489ac2b0
Sascha Steinbiss <sascha@steinbiss.name>
2015-08-09 23:34:54 +0100

    adapt reference parser for new refdir format

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -12,12 +12,6 @@ class Reference < ActiveFile::Base
       ref_key_map = {"pep" => :pepfile, "genome" => :ref_seq, "gaf" => :ref_gaf,
                      "chromosome_pattern" => :abacas_chr_pattern,
                      "chromosomes" => :ref_chr, "gff" => :ref_annot}
-      # TODO make this configurable in the reference definition
-      augustus_ref = {"LmjF" => "leishmania_major_sampled",
-                      "LmjF.1" => "leishmania_major_sampled",
-                      "LbrM" => "leishmania_braziliensis_sampled",
-                      "Tb927" => "trypanosoma_brucei_927_sampled",
-                      "TcCLB" => "trypanosoma_cruzi_sampled"}
       jsondata = File.open("#{CONFIG['referencedir']}/references.json").read
       json = JSON.parse(jsondata)
       out = []
@@ -32,8 +26,7 @@ class Reference < ActiveFile::Base
             newhash[k2] = v2
           end
         end
-        if augustus_ref.has_key?(newhash[:abbr]) then
-          newhash[:augustus_species] = augustus_ref[newhash[:abbr]]
+        if newhash.has_key?('augustus_model') then
           out << newhash
           i += 1
         end
