fe2269996c3280d638cddc81e79a8807fb7f80dc
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-09-15 16:02:31 +0100

    rename Kingpin->Companion

--- config/initializers/custom_config.rb
+++ config/initializers/custom_config.rb
@@ -1,17 +1,17 @@
-CONFIG = YAML.load_file("#{Rails.root.to_s}/config/kingpin.yml")[Rails.env]
+CONFIG = YAML.load_file("#{Rails.root.to_s}/config/companion.yml")[Rails.env]
 
 if not File.exist?("#{CONFIG['nextflowpath']}/nextflow") then
-  raise "Nextflow engine not available in #{Rails.env}! Check config file at #{Rails.root.to_s}/config/kingpin.yml"
+  raise "Nextflow engine not available in #{Rails.env}! Check config file at #{Rails.root.to_s}/config/companion.yml"
 end
 
 if not File.exist?("#{CONFIG['locationconfig']}") then
-  raise "Missing location configuration for #{Rails.env}! Check config file at #{Rails.root.to_s}/config/kingpin.yml"
+  raise "Missing location configuration for #{Rails.env}! Check config file at #{Rails.root.to_s}/config/companion.yml"
 end
 
 if not File.exist?("#{CONFIG['nextflowscript']}") then
-  raise "Missing nextflow script for #{Rails.env}! Check config file at #{Rails.root.to_s}/config/kingpin.yml"
+  raise "Missing nextflow script for #{Rails.env}! Check config file at #{Rails.root.to_s}/config/companion.yml"
 end
 
 if not system("gt -version") then
-#  raise "gt binary not found in PATH! Please install and/or set the correct path to find it."
+  raise "gt binary not found in PATH! Please install and/or set the correct path to find it."
 end
