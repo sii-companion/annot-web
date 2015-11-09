d15b2febed6c2dbfe4db18fb432f72fb1f4865fe
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-11-09 15:48:11 +0000

    add mailer initializer

---
+++ config/initializers/mailer.rb
@@ -0,0 +1,17 @@
+Rails.application.configure do
+  config.action_mailer.default_url_options = {
+    :host => 'localhost',
+    :port => 8000
+  }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'localhost',
+    port:                 465,
+    domain:               'localhost',
+    user_name:            'ss34',
+    password:             '***REMOVED***',
+    authentication:       :login,
+    enable_starttls_auto: true,
+    ssl: true  }
+  end
