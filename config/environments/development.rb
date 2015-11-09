8157feac1a714df13956a4ba7de0d02558cd171e
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-11-09 15:49:52 +0000

    remove email info from default configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,19 +39,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = {
-    :host => 'localhost',
-    :port => 8000
-  }
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'localhost',
-    port:                 465,
-    domain:               'localhost',
-    user_name:            'ss34',
-    password:             '***REMOVED***',
-    authentication:       :login,
-    enable_starttls_auto: true,
-    ssl: true  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,19 +77,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-    config.action_mailer.default_url_options = {
-    :host => 'localhost',
-    :port => 8000
-  }
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'localhost',
-    port:                 465,
-    domain:               'localhost',
-    user_name:            'ss34',
-    password:             '***REMOVED***',
-    authentication:       :login,
-    enable_starttls_auto: true,
-    ssl: true  }
 end
