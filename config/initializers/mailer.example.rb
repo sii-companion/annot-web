a875e8489d7545477fcaad8e0f448d3b245bd478
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-12-04 11:06:20 +0000

    replace mailer settings by example

--- config/initializers/mailer.example.rb
+++ config/initializers/mailer.example.rb
@@ -6,11 +6,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'localhost',
+    address:              'mail.sanger.ac.uk',
     port:                 465,
-    domain:               'localhost',
-    user_name:            'ss34',
-    password:             '***REMOVED***',
+    domain:               'sanger.ac.uk',
+    user_name:            'companion',
+    password:             '###REMOVED###',
     authentication:       :login,
     enable_starttls_auto: true,
     ssl: true  }
