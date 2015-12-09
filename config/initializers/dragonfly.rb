257025325c16e7157de01aee190aa2aaf3791e14
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-12-09 14:37:21 +0000

    fix logger naming

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -4,6 +4,7 @@ class GFF3Analyser
   def call(content)
     path = content.path
     rval = system("gt gff3 -retainids -tidy -show no #{path}")
+    Rails.logger.info "GFF3 is valid: " + rval.to_s
     rval
   end
 end
@@ -12,6 +13,7 @@ class SequenceValidAnalyser
   def call(content)
     path = content.path
     rval = system("gt convertseq -noseq #{path}")
+    Rails.logger.info "sequence #{path} is valid: #{rval.to_s}"
     rval
   end
 end
