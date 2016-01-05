51835710aa68aadf1d6aaa15b9aefc18676fddb7
Sascha Steinbiss <ss34@sanger.ac.uk>
2016-01-05 10:48:46 +0000

    read reCAPTCHA keys from central configuration file

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,9 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = '***REMOVED***'
-  config.private_key = '***REMOVED***'
-  # Uncomment the following line if you are using a proxy server:
-  # config.proxy = 'http://myproxy.com.au:8080'
-  # Uncomment if you want to use the newer version of the API,
-  # only works for versions >= 0.3.7:
-  # config.api_version = 'v2'
+  config.public_key  = CONFIG['recaptcha_pub_key']
+  config.private_key = CONFIG['recaptcha_priv_key']
 end
