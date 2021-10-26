85d6f175322cb4be6410bfaf0b1851655136dd4c
Will Haese-Hill <William.Haese-Hill@glasgow.ac.uk>
2021-10-26 14:31:23 +0100

    increase max sequences to 16000

--- app/models/sequence_file.rb
+++ app/models/sequence_file.rb
@@ -1,6 +1,6 @@
 class SequenceFile < UserFile
   validates_property :valid_sequence, of: :file, as: true, message: "is not a valid DNA sequence file."
-  validates_property :number_of_sequences, of: :file, in: (0..15000), message: "contains more than 15000 sequences."
+  validates_property :number_of_sequences, of: :file, in: (0..16000), message: "contains more than 15000 sequences."
   # TODO: need to validate that it is pure DNA sequence
   # TODO: need to validate that headers are correctly formatted
 end
