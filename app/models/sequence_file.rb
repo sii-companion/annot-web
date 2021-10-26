e88bf7df786ee27821c9de74c6df7fe2d80863c6
Will Haese-Hill <William.Haese-Hill@glasgow.ac.uk>
2021-10-26 15:16:16 +0100

    Up seq num to 30k

--- app/models/sequence_file.rb
+++ app/models/sequence_file.rb
@@ -1,6 +1,6 @@
 class SequenceFile < UserFile
   validates_property :valid_sequence, of: :file, as: true, message: "is not a valid DNA sequence file."
-  validates_property :number_of_sequences, of: :file, in: (0..20000), message: "contains more than 20000 sequences."
+  validates_property :number_of_sequences, of: :file, in: (0..30000), message: "contains more than 30000 sequences."
   # TODO: need to validate that it is pure DNA sequence
   # TODO: need to validate that headers are correctly formatted
 end
