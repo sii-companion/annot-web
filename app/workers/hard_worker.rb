0586a80af1ff29779e3470c0dce38e6d39c0904c
Will Haese-Hill <wahhill@gmail.com>
2023-04-18 11:45:49 +0100

    outsource output creation methods to lib

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -1,7 +1,10 @@
+require 'job_outputs'
+
 class HardWorker
   include Sidekiq::Worker
   sidekiq_options :retry => 0
   include Sidekiq::Status::Worker
+  include JobOutputs
 
   def with_environment(variables={})
     if block_given?
@@ -216,32 +219,33 @@ class HardWorker
           Gene.import genes, on_duplicate_key_ignore: true
         end
 
-        # import clusters
-        if File.exist?("#{job.job_directory}/orthomcl_out") then
-          clusters = []
-          File.open("#{job.job_directory}/orthomcl_out").each_line do |l|
-            m = l.match(/^(OG[0-9]+):\s+(.+)/)
-            next unless m
-            c = Cluster.find_or_create_by(:cluster_id => m[1], :job => job)
-            tr = m[2].scan(/[^|]+\|([^ )]+)/)
-            suffix_patterns = File.readlines("#{Rails.root.to_s}/config/transcript_suffix_patterns.txt", chomp: true)
-            tr.each do |memb|
-              memb_id = memb[0].gsub(/#{suffix_patterns.join("|")}/,"")
-              g = Gene.where([
-                "gene_id LIKE ? AND ((job_id = #{job[:id]} AND species = '#{job[:prefix]}')" \
-                "OR (job_id IS NULL AND species = '#{r[:abbr]}'))", "#{memb_id}%"
-              ]).take
-              if g then
-                unless g.in?(c.genes)
-                  c.genes << g
-                end
-              else
-                Rails.logger.info("#{memb[0]}: #{memb_id} (with job ID #{job[:id]}) not found!")
-              end
-            end
-            c.save!
-          end
-        end
+        # # import clusters
+        # if File.exist?("#{job.job_directory}/orthomcl_out") then
+        #   clusters = []
+        #   File.open("#{job.job_directory}/orthomcl_out").each_line do |l|
+        #     m = l.match(/^(OG[0-9]+):\s+(.+)/)
+        #     next unless m
+        #     c = Cluster.find_or_create_by(:cluster_id => m[1], :job => job)
+        #     tr = m[2].scan(/[^|]+\|([^ )]+)/)
+        #     suffix_patterns = File.readlines("#{Rails.root.to_s}/config/transcript_suffix_patterns.txt", chomp: true)
+        #     tr.each do |memb|
+        #       memb_id = memb[0].gsub(/#{suffix_patterns.join("|")}/,"")
+        #       g = Gene.where([
+        #         "gene_id LIKE ? AND ((job_id = #{job[:id]} AND species = '#{job[:prefix]}')" \
+        #         "OR (job_id IS NULL AND species = '#{r[:abbr]}'))", "#{memb_id}%"
+        #       ]).take
+        #       if g then
+        #         unless g.in?(c.genes)
+        #           c.genes << g
+        #         end
+        #       else
+        #         Rails.logger.info("#{memb[0]}: #{memb_id} (with job ID #{job[:id]}) not found!")
+        #       end
+        #     end
+        #     c.save!
+        #   end
+        # end
+        import_clusters(job)
 
         # store tree files
         if File.exist?("#{job.job_directory}/tree_selection.genes") and
